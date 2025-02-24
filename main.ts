a92b4698a1daa04a746c499f87e7cfc4029d65bc
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-02-24 20:35:49 +0100

    removed src folder. added a Async saveToFile function that takes file name and data and creates a new file

---
+++ main.ts
@@ -0,0 +1,24 @@
+import {
+  reverseString,
+  countCharacters,
+  upperLower,
+} from "./utils/stringUtils.js";
+import path from "path";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const sentence = "Node is good for everything :)";
+
+const reversedSentence = reverseString(sentence);
+
+const saveToFile = async (fileName: string, data: string): Promise<void> => {
+  try {
+    await fs.writeFile(fileName, data);
+    console.log(path.join(__dirname, fileName));
+  } catch (error) {
+    console.error(`Error writing file ${error}`);
+  }
+};
+
+saveToFile("reversed.txt", reversedSentence);
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import {
-  reverseString,
-  countCharacters,
-  upperLower,
-} from "../utils/stringUtils.js";
-
-console.log(reverseString("Hello"));
-console.log(countCharacters("Hello"));
-console.log(upperLower("monster"));
