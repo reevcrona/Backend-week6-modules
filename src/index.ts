92217e250fc53d23c279a95258d198c67a0ad5d3
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-02-24 20:11:40 +0100

    added conCharacters function and upperLower function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import { reverseString } from "../utils/stringUtils.js";
+import {
+  reverseString,
+  countCharacters,
+  upperLower,
+} from "../utils/stringUtils.js";
 
 console.log(reverseString("Hello"));
+console.log(countCharacters("Hello"));
+console.log(upperLower("monster"));
--- utils/stringUtils.ts
+++ utils/stringUtils.ts
@@ -1,3 +1,14 @@
 export const reverseString = (input: string): string => {
   return input.split("").reverse().join("");
 };
+export const countCharacters = (input: string): number => input.length;
+
+export const upperLower = (input: string): string => {
+  let newString: string = "";
+  for (let i = 0; i < input.length; i++) {
+    const upperOrLower =
+      i % 2 === 0 ? input[i].toUpperCase() : input[i].toLowerCase();
+    newString += upperOrLower;
+  }
+  return newString;
+};
